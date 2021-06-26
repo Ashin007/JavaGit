4bc0fe3d66ef0af87b6aab04dba3bc95b4db5677
Ashin007 <ashinvijayan007@gmail.com>
2021-06-26 23:24:53 +0530

    commit from american guy

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Initial commit from master");
+        System.out.println("commit from american guy");
     }
 }
