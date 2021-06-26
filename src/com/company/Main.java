8bf32f0694df3224a10cebd153b6a24933a93f18
Ashin007 <ashinvijayan007@gmail.com>
2021-06-26 23:31:53 +0530

    created new branch

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Initial commit from master");
         System.out.println("commit from american guy");
+
+        System.out.println("American guy is creating a branch called develop");
     }
 }
